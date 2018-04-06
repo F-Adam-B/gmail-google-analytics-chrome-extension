211bb0ffcffbabcdc8cfa95cb1811de665498836
Adam Bradbury <f.adambrad@gmail.com>
2018-04-06 13:15:33 -0600

    added google analytics pixel tracker with initial parameters

---
+++ JS/asyncTrackingCode.js
@@ -0,0 +1,14 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-117096746-1']);
+_gaq.push(['_trackPageview']);
+
+// need to add onClick function once email is opened
+
+(function() {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
---
+++ JS/background.js
@@ -0,0 +1,6 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-117096746-1']);
+_gaq.push(['_trackPageview']);
+
+// EVENT LISTENERS FOR EVENT_PAGE FIRING
+chrome.runtime.onInstalled.addListener(function() {});
